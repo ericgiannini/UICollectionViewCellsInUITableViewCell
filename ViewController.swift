044c78f980a87ec185890ac2ec7cef85609369c9
ericgiannini <eric.giannini@yahoo.com>
2017-03-30 11:41:00 -0700

    Adding project

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  UICollectionViewCellsInUITableViewCell
+//
+//  Created by Eric Giannini on 3/29/17.
+//  Copyright © 2017 Unicorn Mobile, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier: String = "tableViewCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        return cell!
+    }
+}
+
